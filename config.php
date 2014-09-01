7c6e05ab58f566f8f4e4e04e8f9b584fc38bbe02
tc-mccarthy <tc.mccarthy1@gmail.com>
2014-08-31 22:27:40 -0400

    Adds delete support

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 	'services' => array(
 		'default_settings' => array(
 			'params' => array(
-				'key' => 'AKIAI7QKHXIAWLZ6VBGA',
-				'secret' => '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',
+				'key' => 'AKIAIQ3AWRJ3R7JD7MKA',
+				'secret' => '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',
  				// OR: 'profile' => 'my_profile',
 				'region' => 'us-east-1'
 			)
