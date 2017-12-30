911bd5505d906a262c1d9dfc4b9c1b6bae180cf9
TC McCarthy <tc.mccarthy1@gmail.com>
2017-12-30 14:36:12 -0500

    Begins work on regenerating thumbnails

--- inc/wp_ajax.class.php
+++ inc/wp_ajax.class.php
@@ -10,6 +10,7 @@ class tcs3_ajax
         add_action("wp_ajax_get_attachment_ids", [$this, "get_attachment_ids"]);
         add_action("wp_ajax_sync_all", [$this, "sync_all"]);
         add_action("wp_ajax_mark_all_synced", [$this, "mark_all_synced"]);
+        add_action("wp_ajax_regenerate_all_thumbnails", [$this, "regenerate_all_thumbnails"]);
     }
 
     public function push_single()
@@ -88,5 +89,55 @@ class tcs3_ajax
 
         echo json_encode($attachments);
         exit();
+    }
+
+    public function regenerate_all_thumbnails()
+    {
+        global $tcS3, $wpdb;
+
+        $attachments = $wpdb->get_results("SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key LIKE '%attachment_meta%'");
+
+        $attachment_ids = array_map(function ($o) {
+            return $o->post_id;
+        }, $attachments);
+
+        foreach ($attachment_ids as $id) {
+            $url = wp_get_attachment_image_url($id, "original");
+            $file = $this->download_image($url);
+            wp_generate_attachment_metadata($id, $file);
+        }
+    }
+
+    public function download_image($url)
+    {
+        global $tcS3;
+
+        set_time_limit(0);
+
+        $url_path = $tcS3->base_->options["local_path"] . "/" . str_replace($tcS3->base_->options["s3_url"], "", $url);
+        $file_path = dirname($url_path);
+
+        error_log("PATH: " . $url_path);
+        error_log("FILE PATH: " . $file_path);
+
+        mkdir($file_path, 0777, true);
+
+        //This is the file where we save the    information
+        $fp = fopen($url_path, 'w+');
+
+        //Here is the file we are downloading, replace spaces with %20
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+
+        // write curl response to file
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        // get curl response
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+
+        return $url_path;
     }
 }
